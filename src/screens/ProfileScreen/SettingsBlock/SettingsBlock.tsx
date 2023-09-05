d5d0e019673217756c0c4911cfea8e1d7bc5c943
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-05 16:24:00 +0300

    chore: remove ChangePasswordForm

--- src/screens/ProfileScreen/SettingsBlock/SettingsBlock.tsx
+++ src/screens/ProfileScreen/SettingsBlock/SettingsBlock.tsx
@@ -1,8 +1,6 @@
 import React, { memo } from 'react';
 import cn from 'clsx';
-import { Divider } from 'antd';
 import { ProfileCompletedForm } from '../ProfileCompletedForm';
-import { ChangePasswordCompletedForm } from '../ChangePasswordCompletedForm';
 import s from './SettingsBlock.sass';
 
 export type SettingsBlockProps = {
@@ -12,8 +10,6 @@ export type SettingsBlockProps = {
 export const SettingsBlock = memo<SettingsBlockProps>(({ className }) => (
   <div className={cn(s.root, className)}>
     <ProfileCompletedForm />
-    <Divider />
-    <ChangePasswordCompletedForm />
   </div>
 ));
 
