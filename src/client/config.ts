cd3074c65e2951cacc38262b91db1582c08115b9
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-07 22:51:03 +0300

    chore(src/client/config): use new ip6

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,1 +1,1 @@
-export const URL = process.env.LOCAL === 'true' ? 'http://localhost:4001' : 'http://ac4366b428e4.vps.myjino.ru';
+export const URL = process.env.LOCAL === 'true' ? 'http://localhost:4001' : 'http://otus.websockets.lesson.ru';
